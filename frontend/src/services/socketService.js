45720a778a8b0bf6013de4788acf395ca64b54ce
Jatin Agrawal <jatinagrawal0987654321@gmail.com>
2025-06-18 20:58:07 +0530

    redeploying

--- frontend/src/services/socketService.js
+++ frontend/src/services/socketService.js
@@ -1,6 +1,6 @@
 // frontend/src/services/socketService.js
 import { io } from 'socket.io-client';
-const BACKEND_URL =  'http://localhost:3000';
+const BACKEND_URL =  VITE_BACKEND_URL;
 
 let socket = null;
 export const connectSocket = () => {
